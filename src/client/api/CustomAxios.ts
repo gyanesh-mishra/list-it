5850a33d31d6dfe38ad993b22a823fa8cf1201f7
Gyanesh Mishra <gyan@freshworks.io>
2019-12-01 15:55:42 -0800

    update custom axios

--- src/client/api/CustomAxios.ts
+++ src/client/api/CustomAxios.ts
@@ -4,11 +4,15 @@ import axios from 'axios';
 CustomAxios helps implement interceptors at the request level
 before it even reaches any of the handlers.
 
-Currently if the server returns any error codes over 500, we retry the request every 2 seconds until success.
+Currently if the server returns any error codes over 500, we retry the request every 2 seconds
+until success
 
 // TODO: Implement axios-retry library once the custom config is fixed.
 Alternatively, Implement custom exponential back-off once the custom config flags are fixed.
+Instead of using the request header hack. https://github.com/axios/axios/issues/2203
 */
+
+const MAX_RETRIES = 5;
 
 const instance = axios.create({});
 
@@ -27,9 +31,16 @@ instance.interceptors.response.use(
       config,
       response: { status },
     } = error;
-    const originalRequest = config;
 
-    if (status >= 500) {
+    // If the request has failed check if it's the first failure, if not, note the failure count.
+    const originalRequest = config;
+    const current_retry_count =
+      originalRequest.headers.retry_count === undefined ? 1 : originalRequest.headers.retry_count + 1;
+    originalRequest.headers.retry_count = current_retry_count;
+
+    // If the request has been re-tried more than the max set number, stop retrying and reject the promise.
+    let retry = current_retry_count >= MAX_RETRIES ? false : true;
+    if (status >= 500 && retry) {
       return sleepRequest(2000, originalRequest);
     } else {
       return Promise.reject(error);
