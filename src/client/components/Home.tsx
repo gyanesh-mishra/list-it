c5c66cdd836e8472149a7469623695e5d33759a6
Gyanesh Mishra <gyan@freshworks.io>
2019-11-30 17:40:26 -0800

    add line breaks to home page

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -7,9 +7,12 @@ export class Home extends React.Component<any> {
       <div>
         <h2>Welcome!</h2>
         <p>
-          This is the home of best List makers. To go to your list, remember the ID and go to your browser and type: For
-          e.g. if your ID was 'winston234' http://localhost:3000/list/winston234 Yeah we can't afford hosting
-          ¯\_(ツ)_/¯, BUT... only you can access your data! How cool is that? Definitely a feature.
+          This is the home of best List makers. <br /> To go to your list, remember the ID and go to your browser and
+          type the URL.
+          <br /> For e.g. if your ID was 'winston234', the URL would be http://localhost:3000/list/winston234.
+          <br /> Yeah we can't afford hosting ¯\_(ツ)_/¯, BUT... only you can access your data!
+          <br />
+          How cool is that? Definitely a feature.
         </p>
       </div>
     );
